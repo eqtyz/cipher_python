ba83bd042d2dba19fb6018c4b5f79cbd29bf4de3
Kategov Dmitry <kategov.di@corp.kvantorium54.ru>
2023-12-13 19:24:58 +0700

    added main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print(Hello world)
+from string import ascii_letters
+
+ALPHABET = list(ascii_letters)
+
+message = input("Введите сообщение: ")
+key = input("Введите приватный ключ: ")
+
+key_message = (key * 10)[:len(message)]
+encrypted_message = ''
+
+for i in range(len(message)):
+    letter_index_message = ALPHABET.index(message[i])
+    letter_index_key = ALPHABET.index(key_message[i])
+    letter_index = letter_index_message + letter_index_key
+
+    if letter_index > len(ALPHABET):
+        letter_index -= len(ALPHABET)
+
+    encrypted_message += ALPHABET[letter_index]
+
+print(f'Зашифрованное сообщение: {encrypted_message}')
